605d5aeae7c1fbc903705cc23c0f6e98b2d6dbfd
Seigneur Gaëtan <72962599+gaetanarinfo@users.noreply.github.com>
2021-04-27 11:29:00 +0200

    Update MailboxType.php

--- src/Form/MailboxType.php
+++ src/Form/MailboxType.php
@@ -4,10 +4,8 @@ namespace App\Form;
 
 use App\Entity\Mailbox;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -22,6 +20,15 @@ class MailboxType extends AbstractType
                     'class' => 'form-control col-md-12',
                     'style' => 'height: 200px;'
                 ]
+            ])
+            ->add('author', HiddenType::class, [
+                'label' => false,
+            ])
+            ->add('subject', HiddenType::class, [
+                'label' => false,
+            ])
+            ->add('recipient', HiddenType::class, [
+                'label' => false,
             ])
             ;
     }
